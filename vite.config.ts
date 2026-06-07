5700aa83e22a11b48cbf499ecec5002ae43bf9ec
recursive-reshy <61969021+recursive-reshy@users.noreply.github.com>
2026-06-07 10:46:12 +0800

    feat: Added aliases

--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,7 @@
 import { defineConfig } from 'vite'
 import react, { reactCompilerPreset } from '@vitejs/plugin-react'
 import babel from '@rolldown/plugin-babel'
+import path from 'path'
 
 // https://vite.dev/config/
 export default defineConfig( {
@@ -8,4 +9,16 @@ export default defineConfig( {
     react(),
     babel( { presets: [ reactCompilerPreset() ] } )
   ],
+  resolve: {
+    alias: {
+      '@api': path.resolve( __dirname, 'src/api' ),
+      '@assets': path.resolve( __dirname, 'src/assets' ),
+      '@components': path.resolve( __dirname, 'src/components' ),
+      '@pages': path.resolve( __dirname, 'src/pages' ),
+      '@store': path.resolve( __dirname, 'src/store' ),
+      '@theme': path.resolve( __dirname, 'src/theme' ),
+      '@types': path.resolve( __dirname, 'src/types' ),
+      '@': path.resolve( __dirname, 'src' )
+    }
+  }
 } )
