e329adcc5eed8b2d57dfad06d4f26958bd6e66f1
recursive-reshy <61969021+recursive-reshy@users.noreply.github.com>
2026-06-10 14:33:15 +0800

    feat: CssBaseline

--- src/main.tsx
+++ src/main.tsx
@@ -1,13 +1,19 @@
+// React
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.tsx'
+// Redux
 import { Provider } from 'react-redux'
+// Mui
+import { CssBaseline } from '@mui/material'
+// App
+import App from './App.tsx'
+import './index.css'
 import { store } from '@store/store.ts'
 
 createRoot( document.getElementById( 'root' )! ).render(
   <StrictMode>
     <Provider store={ store }>
+      <CssBaseline />
       <App />
     </Provider>
   </StrictMode>,
